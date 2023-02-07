118984593d255e0d4c0b948e88ee0e055e7e3f31
benjamindiz <benjamin.diz@gmail.com>
2023-02-07 11:16:14 +0100

    corrección de errores
    corrección de errores

--- programa/src/main/java/com/ibm/prueba/programa/Prueba.java
+++ programa/src/main/java/com/ibm/prueba/programa/Prueba.java
@@ -1,5 +1,6 @@
 package com.ibm.prueba.programa;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -10,18 +11,30 @@ import com.ibm.prueba.programa.util.JPAUtil;
 
 public class Prueba {
 
+	static String pattern = "dd/MM/yyyy";
+	static SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+
 	public static void main(String[] args) {
+		if (args.length != 1)
+			System.out.println("Es obligatorio informar el código del cliente");
+		else {
+			Long idCliente = Long.parseLong(args[0]);
 
-		Long idCliente = Long.parseLong(args[0]);
-
-		EntityManager em = JPAUtil.getEntityManager();
-		ProveedorDao proveedorDao = new ProveedorDao(em);
-		List<Proveedor> proveedores = proveedorDao.buscarPorCliente(idCliente);
-		
-		proveedores.stream().forEach((p) -> {
-			System.out.println(p.getIdCliente() + "," + p.getNombre() + ","  + p.getFechaDeAlta() + "," + p.getIdCliente());
-	});
-
+			EntityManager em = JPAUtil.getEntityManager();
+			ProveedorDao proveedorDao = new ProveedorDao(em);
+			List<Proveedor> proveedores = proveedorDao.buscarPorCliente(idCliente);
+			if (proveedores.isEmpty())
+				System.out.println("El cliente no tiene proveedores asignados.");
+			else {
+				System.out.println("Proveedores:");
+				System.out.println("id_proveedor, nombre, fecha de alta, id_cliente");
+				proveedores.stream().forEach((p) -> {
+					String fechaDeAlta = simpleDateFormat.format(p.getFechaDeAlta());
+					System.out.println(
+							p.getIdProveedor() + "," + p.getNombre() + "," + fechaDeAlta + "," + p.getIdCliente());
+				});
+			}
+			em.close();
+		}
 	}
-
 }
