b5e71c283586f33abb66e001ba149961795fb5ed
benjamindiz <benjamin.diz@gmail.com>
2023-02-07 10:44:51 +0100

    carga inicial de datos
    crear la carga inicial de datos en la base de datos

--- programa/src/main/java/com/ibm/prueba/programa/util/JPAUtil.java
+++ programa/src/main/java/com/ibm/prueba/programa/util/JPAUtil.java
@@ -1,5 +1,14 @@
 package com.ibm.prueba.programa.util;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class JPAUtil {
 
+	private static final EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory("teneaibm");
+
+	public static EntityManager getEntityManager() { 
+		return FACTORY.createEntityManager();
+	}
 }
